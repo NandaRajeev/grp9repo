2ec3123a963c35fea05c39ffa3ccd7a0c62a7aed
Hridya K K <hridyaa005@gmail.com>
2026-06-28 14:36:26 +0530

    refactor code structure for improved readability and maintainability

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express=require("express");
+const cors=require("cors");
+const connectDB=require("./config/db");
+
+const authRoutes=require("./routes/authRoutes");
+const bookRoutes=require("./routes/bookRoutes");
+
+const app=express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/books",bookRoutes);
+
+app.get("/",(req,res)=>{
+ res.send("Library Management API Running");
+});
+
+app.listen(process.env.PORT || 5000,()=>{
+ console.log("Server Running");
+});
